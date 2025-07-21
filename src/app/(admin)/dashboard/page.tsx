466dff6f7656d2f2fa7db1580a1ddf8d0c8a4314
Nyasha Chizampeni <nchizampeni@gmail.com>
2025-07-21 22:28:01 +0400

    wip

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -21,7 +21,7 @@ type Order = {
   delivery_status: string;
   payment_status: string;
   payment_method: string;
-  total: string | number;
+  total: number;
 };
 type Driver = {
   id: number | string;
