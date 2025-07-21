f6a519aeddb439f0ad18231943bf56ff14bf9163
Nyasha Chizampeni <nchizampeni@gmail.com>
2025-07-21 22:48:46 +0400

    wip

--- src/components/form/form-elements/FileInputExample.tsx
+++ src/components/form/form-elements/FileInputExample.tsx
@@ -5,12 +5,7 @@ import ComponentCard from "../../common/ComponentCard";
 import Label from "../Label";
 
 export default function FileInputExample() {
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      console.log("Selected file:", file.name);
-    }
-  };
+  
 
   return (
     <ComponentCard title="File Input">
